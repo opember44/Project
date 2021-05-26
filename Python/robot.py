7adfcca2c7576ffd2a04ce51af2b6bf119517ae7
Caden <ctucker02@charlottesvilleschools.org>
2021-05-26 15:21:53 +0100

    yay, a working servo!

--- Python/robot.py
+++ Python/robot.py
@@ -1,1 +1,33 @@
 # my roobt code
+
+# Servo Control
+import time
+import wiringpi
+
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+
+# set #18 to be a PWM output
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+# divide down clock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+
+delay_period = 0.01
+
+print("running!")
+
+while True:
+	# wiringpi.pwmWrite(18,150)
+	
+        for pulse in range(50, 180, 1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
+        for pulse in range(180, 50, -1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
+	
